9aa7cadfcf11069b99ece87e508ac77a7b6af1ac
james-plw <james.wright2@bath.edu>
2025-09-04 11:39:43 +0100

    Fetch kickoff tests

--- test/test_bbc_scrape.py
+++ test/test_bbc_scrape.py
@@ -1,9 +1,11 @@
 # pylint: skip-file
 import pytest
+import datetime
 from bs4 import BeautifulSoup
 
 from src.bbc_scrape import (parse_match_stats, extract_team_names,
-                            find_first_float, get_stats_from_arr, extract_hidden_stat)
+                            find_first_float, get_stats_from_arr,
+                            extract_hidden_stat, fetch_kickoff_time)
 
 
 def test_parse_match_stats():
@@ -34,16 +36,16 @@ def test_parse_match_stats():
 def test_extract_team_names_works():
     html = """
     <section aria-labelledby="basic-match-stats">
-      <div>Shots TeamA 10 TeamB 12</div>
+      <div>Shots TeamA 10 Team B 12</div>
     </section>
     """
     soup = BeautifulSoup(html, "html.parser")
     section = soup.select_one("section[aria-labelledby='basic-match-stats']")
     result = extract_team_names(section)
     assert result["Home Team"] == "TeamA"
-    assert result["Away Team"] == "TeamB"
+    assert result["Away Team"] == "Team B"
     assert result["Home Length"] == 1
-    assert result["Away Length"] == 1
+    assert result["Away Length"] == 2
 
 
 def test_extract_team_names_raises_error():
@@ -104,3 +106,29 @@ def test_extract_hidden_stat_returns_none_if_no_spans():
     html = "<div>No spans here</div>"
     soup = BeautifulSoup(html, "html.parser")
     assert extract_hidden_stat(soup) is None
+
+
+def test_fetch_kickoff_time_valid_datetime():
+    html = """
+    <html>
+      <body>
+        <time>Sat 14 Sep 2025</time>
+        <time>15:00</time>
+      </body>
+    </html>
+    """
+    dt = fetch_kickoff_time(html)
+    assert isinstance(dt, datetime.datetime)
+    assert dt.year == 2025
+    assert dt.hour == 15
+    assert dt.minute == 0
+
+
+def test_fetch_kickoff_time_missing_date():
+    html = "<html><body><time>15:00</time></body></html>"
+    assert fetch_kickoff_time(html) is None
+
+
+def test_fetch_kickoff_time_missing_time():
+    html = "<html><body><time>Sat 14 Sep 2025</time></body></html>"
+    assert fetch_kickoff_time(html) is None
